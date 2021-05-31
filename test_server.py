b5ea732eb3bd46ca4f3fa2099cf25f8c1fbce8ec
Raphael LOSSEC <solmyr-ars@laposte.net>
2021-05-31 12:00:12 +0200

    tests booking with enough and not enought clubs points

--- test_server.py
+++ test_server.py
@@ -1,63 +1,32 @@
-import server
-from server import app
 
-
-test_clubs = [
-        {
-            "id": 1,
-            "name": "Simply Lift",
-            "email": "john@simplylift.co",
-            "points": "13"
-        },
-        {
-            "id": 2,
-            "name": "Iron Temple",
-            "email": "admin@irontemple.com",
-            "points": "4"
-        },
-        {   
-            "id": 3,
-            "name": "She Lifts",
-            "email": "kate@shelifts.co.uk",
-            "points": "12"
-        }
-    ]
-
-
-test_competitions = [
-        {
-            "id": 1,
-            "name": "Spring Festival",
-            "date": "2021-08-27 10:00:00",
-            "numberOfPlaces": "25"
-        },
-        {
-            "id": 2,
-            "name": "Fall Classic",
-            "date": "2020-10-22 13:30:00",
-            "numberOfPlaces": "13"
-        }
-    ]
+from server import app, load_clubs, load_competitions, get_club, get_competition
+import pytest
 
 
 
-class TestIndex:
-    test_client = app.test_client()
+class Test:
+    def setup_method(self):
+        self.clubs = load_clubs()
+        self.competitions = load_competitions()
+        self.test_client = app.test_client()
 
-    def test_get_page(self):
+    def teardown_method(self):
+        pass
+
+    def test_get_index(self):
         response = self.test_client.get('/', content_type='html/text')
         assert response.status_code == 200
 
-
-class TestShowSummary:
-    server.clubs = test_clubs
-    server.competitions = test_competitions
-    test_client = app.test_client()
-
-    def test_input_known_email(self):
-        response = self.test_client.post('/showSummary', data=dict(email="admin@irontemple.com"))
-        assert response.status_code == 200
+    @pytest.mark.parametrize("email, status", [("admin@irontemple.com", 200), ("admin@iroemple.com", 403)])
+    def test_input_email(self, email, status):
+        response = self.test_client.post('/showSummary', data=dict(email=email))
+        assert response.status_code == status
     
-    def test_input_unknown_email(self):
-        response = self.test_client.post('/showSummary', data=dict(email="admin@iroemple.com"))
-        assert response.status_code == 403
+    @pytest.mark.parametrize("club_id, competition_id, number_of_places, club_points, competition_places, messages", [((1, 1, 14, 13, 25, ["<li>You don&#39;t have enough points. Try with a number smaller than your total points.</li>"])), (1, 1, 11, 2, 14, ["<li>Great-booking complete!</li>"])])
+    def test_booking(self, club_id, competition_id, number_of_places, club_points, competition_places, messages):
+        response = self.test_client.post('/purchasePlaces', data=dict(club=club_id, competition=competition_id, places=number_of_places))
+        assert response.status_code == 200
+        assert str.encode(f"Points available: {club_points}") in response.data
+        assert str.encode(f"Number of Places: {competition_places}") in response.data
+        for message in messages:
+            assert str.encode(message) in response.data
